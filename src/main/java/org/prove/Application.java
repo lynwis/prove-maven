c33f44d941f21c03c00b4df7f38523667caff960
Marco Magnatti <marco.magnatti@gmail.com>
2017-05-04 16:26:05 +0200

    change pom.xml

--- src/main/java/org/prove/Application.java
+++ src/main/java/org/prove/Application.java
@@ -1,13 +1,29 @@
 package org.prove;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
+	
+	public List<String> lista;
     
     public Application() {
         System.out.println ("Inside Application");
+		lista = new ArrayList<>();
+		lista.add("Item1");
+		
     }
+	
+	public void listAllItems() {
+		for (String s : lista) {
+			System.out.println(s);
+		}
+	}
 
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
+		app.listAllItems();
+		
     }
 }
